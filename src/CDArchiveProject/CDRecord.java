1e574694455759c4318810bed57bd782aa3aa2c1
Laszlo Varga <laci-vargaaus@hotmail.com>
2020-10-18 22:03:15 +1000

    Console update

--- src/CDArchiveProject/CDRecord.java
+++ src/CDArchiveProject/CDRecord.java
@@ -1,5 +1,8 @@
 package CDArchiveProject;
 
+import java.util.Arrays;
+import java.util.List;
+
 public class CDRecord {
 
 
@@ -12,43 +15,109 @@ public class CDRecord {
     String description;
     boolean onLoan;
 
-    public CDRecord (String title, String author, String section, String description){
+    public CDRecord (String title, String author, String section,int x, int y, int barcode, String description, boolean onLoan){
         this.title=title;
         this.author=author;
         this.section=section;
+        this.xLocation=x;
+        this.yLocation = y;
+        this.barcode=barcode;
         this.description=description;
+        this.onLoan=onLoan;
     }
 
-    public String getAuthor() { return author; }
 
-    public String getTitle() { return title; }
-
-    public String getSection() {return section;}
-
-    public String getDescription() { return description; }
-
-    public boolean isOnLoan() { return onLoan; }
 
     public CDRecord(int barcode){
         this.barcode=barcode;
 
     }
 
-    public CDRecord (int xLocation, int yLocation){
-        this.xLocation = xLocation;
-        this.yLocation = yLocation;
-    }
 
-    public int getxLocation() { return xLocation; }
 
-    public int getyLocation() { return yLocation; }
 
-    public int getBarcode() {
-        return barcode;
-    }
 
     @Override
     public String toString() {
         return Integer.toString(this.barcode);
+    }
+
+    public static List<CDRecord> getTestData() {
+        CDRecord[] records = new CDRecord[]{
+                new CDRecord("foo", "basdar","Bazr",2,0,10101,"Ablmu", false),
+                new CDRecord("ro", "badsar","Bzar",1,0,10201,"Ablmu", false),
+                new CDRecord("daa", "bdasar","Barz",1,1,10401,"Ablmu", false),
+                new CDRecord("fsao", "bsadar","Bdar",1,1,10501,"Ablmu", false),
+                new CDRecord("foas", "bdar","Baar",7,1,16001,"Ablmu", false),
+                new CDRecord("foaso", "baar","Bwar",3,2,10701,"Ablmu", false),
+                new CDRecord("foasdo", "bsar","Bqar",3,1,10801,"Ablmu", false),
+                new CDRecord("fosado", "bar","Bdar",4,2,10501,"Ablmu", false)
+        };
+
+        return Arrays.asList(records);
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public void setSection(String section) {
+        this.section = section;
+    }
+
+    public void setxLocation(int xLocation) {
+        this.xLocation = xLocation;
+    }
+
+    public void setyLocation(int yLocation) {
+        this.yLocation = yLocation;
+    }
+
+    public void setBarcode(int barcode) {
+        this.barcode = barcode;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public void setOnLoan(boolean onLoan) {
+        this.onLoan = onLoan;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public String getSection() {
+        return section;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public boolean isOnLoan() {
+        return onLoan;
+    }
+
+    public int getxLocation() {
+        return xLocation;
+    }
+
+    public int getyLocation() {
+        return yLocation;
+    }
+
+    public int getBarcode() {
+        return barcode;
     }
 }
--- src/Experiments/MainMenu.java
+++ src/Experiments/MainMenu.java
@@ -23,13 +23,13 @@ public class MainMenu {
 
     private void createUI() {
         JLabel searchLabel = new JLabel("Search String:");
-        addComponent(window, searchLabel, 0, 0, 1, 1, 0.1f, 0f);
+        addComponent(window, searchLabel, 0, 0, 1, 1, 0.1f, 0.1f);
 
         JTextField searchstring = new JTextField("");
-        addComponent(window, searchstring, 2, 0, 4, 1, 0.01f, 0f);
+        addComponent(window, searchstring, 2, 0, 1, 1, 2f, 0.1f);
 
         JButton search = new JButton("Search");
-        addComponent(window, search, 7, 0, 0, 1, 0f, 0f);
+        addComponent(window, search, 3, 1, 1, 1, 2f, 0.1f);
 
         JLabel label = new JLabel("Archive CDs");
         addComponent(window, label, 1, 2, 2, 1, 1f, 0f);
@@ -40,7 +40,7 @@ public class MainMenu {
         JTable tbl = new JTable(mdl);
         JScrollPane ArchivePanel = new JScrollPane(tbl);
         ArchivePanel.setBorder(BorderFactory.createLineBorder(Color.BLUE));
-        addComponent(window, ArchivePanel, 0,3,8,2,1f,0);
+        addComponent(window, ArchivePanel, 0,3,6,1,100f,110);
         tbl.getColumnModel().getColumn(0).setPreferredWidth(90);
         tbl.getColumnModel().getColumn(1).setPreferredWidth(80);
         tbl.getColumnModel().getColumn(2).setPreferredWidth(60);
@@ -52,8 +52,25 @@ public class MainMenu {
 
         JPanel ItemPnl = new JPanel();
         ItemPnl.setLayout(new GridBagLayout());
-        addComponent(window,ItemPnl,5,0,1,1,0f,0f );
-        JLabel lbltitle = new JLabel();
+        ItemPnl.setBorder(BorderFactory.createLineBorder(Color.BLACK));
+        addComponent(window,ItemPnl,7,1,1,1,10,0f );
+        JLabel lbltitle = new JLabel("Title:");
+        ItemPnl.add(lbltitle);
+        lbltitle = addComponent(ItemPnl, lbltitle, 0,0,1,1,0.1f,0.1f);
+        JLabel lblAuthor = new JLabel("Author");
+        lblAuthor = addComponent(ItemPnl, lblAuthor, 0,1,1,1,0.1f,0.1f);
+        JLabel lblSection = new JLabel("Section");
+        lblAuthor = addComponent(ItemPnl, lblAuthor, 0,2,2,1,0.1f,0.1f);
+        JLabel lblxLocation = new JLabel("X");
+        lblAuthor = addComponent(ItemPnl, lblAuthor, 0,3,1,1,0.1f,0.1f);
+        JLabel lblyLocation = new JLabel("Y");
+        lblAuthor = addComponent(ItemPnl, lblAuthor, 0,4,1,1,0.1f,0.1f);
+        JLabel lblBarcode = new JLabel("Barcode");
+        lblAuthor = addComponent(ItemPnl, lblAuthor, 0,5,5,1,0.1f,0.1f);
+        JLabel lblDesc = new JLabel("Description");
+        lblAuthor = addComponent(ItemPnl, lblAuthor, 0,6,1,1,0.1f,0.1f);
+        JLabel lblOnLoan = new JLabel("On-Loan");
+        lblAuthor = addComponent(ItemPnl, lblAuthor, 0,7,1,1,0.1f,0.1f);
 
 
 
@@ -72,7 +89,7 @@ public class MainMenu {
     private <C extends Component> C addComponent(Container container,C component, int gridX, int gridY, int gridWidth,
                                                  int gridHeight, float weightX, float weightY){
         GridBagConstraints constraints = new GridBagConstraints();
-        constraints.fill = GridBagConstraints.CENTER;
+        constraints.fill = GridBagConstraints.BOTH;
         constraints.gridx = gridX;
         constraints.gridy = gridY;
         constraints.gridwidth = gridWidth;
